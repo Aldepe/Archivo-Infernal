875b2869400092b6e22d3bb1e927c88eb8537d05
Álvaro Delgado Pérez <72433028+Aldepe@users.noreply.github.com>
2026-06-01 14:49:52 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "";
-export const SUPABASE_ANON_KEY = "";
+export const SUPABASE_URL = "https://guvjbzkbqdnmpjueivyq.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1dmpiemticWRubXBqdWVpdnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyNTg4NjgsImV4cCI6MjA5NTgzNDg2OH0.1H8Z0-CJo9cyhPxAqLco2VyB77dwB51JHaYluN7mFkU";
